7cb0517a28f5b470bcfcf3478455941d6938852d
github username <github email address>
2019-05-22 10:09:05 -0400

    fixed styling

--- src/index.js
+++ src/index.js
@@ -11,30 +11,39 @@ function getShowings(){
 .then(data => {
     console.log(data.showings);// showings
     data.showings.forEach(showing => {
-        let divContent = document.createElement('div');
-        divContent.className = "content";
-            let divHeader = document.createElement('div');
-            divHeader.innerText = showing.film.title;
-            let divRuntime = document.createElement('div');
-            divRuntime.className = "meta";
-            divRuntime.innerText = showing.film.runtime;
-            let divDescription = document.createElement('div');
-            divDescription.className = "description";
-            divDescription.innerText = showing.capacity;
-                let spanShowtime = document.createElement('span');
-                spanShowtime.className = "ui label";
-                spanShowtime.innerText = showing.showtime;
-            divDescription.appendChild(spanShowtime);
-            let divExtraContent = document.createElement('div');
-                let buyTicket = document.createElement('div');
-                buyTicket.className = "ui blue button";
-                buyTicket.innerText = "Buy Ticket";
-            divExtraContent.appendChild(buyTicket);
-        divContent.appendChild(divHeader);
-        divContent.appendChild(divRuntime);
-        divContent.appendChild(divDescription);
-        divContent.appendChild(divExtraContent);
-        cardShowing.appendChild(divContent);
+        createCards(showing);
     })
 })
+}
+
+function createCards(showing){
+    let card = document.createElement('div');
+        card.className = "card";
+            let divContent = document.createElement('div');
+            divContent.className = "content";
+                let divHeader = document.createElement('div');
+                divHeader.innerText = `Title: ${showing.film.title}`;
+                let divRuntime = document.createElement('div');
+                divRuntime.className = "meta";
+                divRuntime.innerText = `Runtime: ${showing.film.runtime}`;
+                let divDescription = document.createElement('div');
+                divDescription.className = "description";
+                // divDescription.innerText = `Capacity: ${showing.capacity}`;
+                    let spanShowtime = document.createElement('span');
+                    spanShowtime.className = "ui label";
+                    spanShowtime.innerText = showing.showtime;
+                divDescription.appendChild(spanShowtime);
+                divDescription.innerHTML += `Capacity: ${showing.capacity}`;
+                let divExtraContent = document.createElement('div');
+                    let buyTicket = document.createElement('div');
+                    buyTicket.className = "ui blue button";
+                    buyTicket.innerText = "Buy Ticket";
+                divExtraContent.appendChild(buyTicket);
+            divContent.appendChild(divHeader);
+            divContent.appendChild(divRuntime);
+            divContent.appendChild(divDescription);
+            divContent.appendChild(divExtraContent);
+            //cardShowing.appendChild(divContent);
+        card.appendChild(divContent);
+        cardShowing.appendChild(card);
 }
